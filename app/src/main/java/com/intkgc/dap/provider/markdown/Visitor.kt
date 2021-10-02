e682717c597cbcaa87f69cdb41c933c9d4eaed82
Kirill <kirbo06@mail.ru>
2021-10-02 11:07:05 +0200

    implement md code blocks parser

--- app/src/main/java/com/intkgc/dap/provider/markdown/Visitor.kt
+++ app/src/main/java/com/intkgc/dap/provider/markdown/Visitor.kt
@@ -1,54 +1,56 @@
 package com.intkgc.dap.provider.markdown
 
+import com.intkgc.dap.page.CodePanel
 import com.intkgc.dap.page.Element
 import com.intkgc.dap.page.Text
 import com.intkgc.dap.page.TextStyle
-import org.commonmark.node.AbstractVisitor
-import org.commonmark.node.Heading
-import org.commonmark.node.Paragraph
-import org.commonmark.node.StrongEmphasis
+import org.commonmark.node.*
 
 class Visitor : AbstractVisitor() {
 
     val elementsList: ArrayList<Element> = ArrayList()
 
-    override fun visit(emphasis: StrongEmphasis?) {
-        if (emphasis != null) {
-            val text = emphasis.firstChild as org.commonmark.node.Text
-            when (emphasis.openingDelimiter) {
-                "__" -> {
-                    elementsList.add(
+    override fun visit(emphasis: StrongEmphasis) {
+        val text = emphasis.firstChild as org.commonmark.node.Text
+        when (emphasis.openingDelimiter) {
+            "__" -> {
+                elementsList.add(
                         Text(text.literal, TextStyle.ITALIC)
-                    )
-                }
-                "**" -> {
-                    elementsList.add(
+                )
+            }
+            "**" -> {
+                elementsList.add(
                         Text(text.literal, TextStyle.BOLD)
-                    )
-                }
+                )
             }
         }
         visitChildren(emphasis)
     }
 
-    override fun visit(heading: Heading?) {
-        if (heading != null) {
-            val text = heading.firstChild as org.commonmark.node.Text
-            elementsList.add(
+    override fun visit(heading: Heading) {
+        val text = heading.firstChild as org.commonmark.node.Text
+        elementsList.add(
                 Text(text.literal, TextStyle.HUGE, TextStyle.BOLD)
-            )
-        }
+        )
         visitChildren(heading)
     }
 
-    override fun visit(text: org.commonmark.node.Text?) {
-        if (text != null) {
-            if (text.parent is Paragraph) {
-                elementsList.add(
+    override fun visit(text: org.commonmark.node.Text) {
+        if (text.parent is Paragraph) {
+            elementsList.add(
                     Text(text.literal)
-                )
-            }
+            )
         }
         visitChildren(text)
+    }
+
+    override fun visit(code: Code) {
+        elementsList.add(CodePanel(code.literal))
+        super.visit(code)
+    }
+
+    override fun visit(fencedCodeBlock: FencedCodeBlock) {
+        elementsList.add(CodePanel(fencedCodeBlock.literal, fencedCodeBlock.info))
+        super.visit(fencedCodeBlock)
     }
 }
